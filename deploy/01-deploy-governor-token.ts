0cbd16e9b4cb792b163c9fcbe330c85143b967ae
Vincent <v3rdugo@yahoo.com>
2022-08-19 15:51:35 -0700

    deploy script working

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -5,7 +5,19 @@ const deployGovernanceToken: DeployFunction = async function (
     hre: HardhatRuntimeEnvironment
 )
 {
-    console.log('deploy governance token')
+    const { getNamedAccounts, deployments, network } = hre
+
+    const { deploy, log } = deployments
+
+    const { deployer } = await getNamedAccounts()
+
+    log('Deploying governance token...')
+    const governanceToken = await deploy('GovernanceToken', {
+        from: deployer,
+        args: [],
+        log: true
+    })
+    log(`Deployed governance token to ${governanceToken.address}`)
 }
 
 export default deployGovernanceToken
